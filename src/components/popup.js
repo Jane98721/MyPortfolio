9c6e049bcad4465fa827fa5ada96750eebabe47e
jane <jane.lehtola@afvagroup.com>
2025-10-23 10:37:05 +0200

    test

--- src/components/popup.js
+++ src/components/popup.js
@@ -6,7 +6,7 @@ function Popup ({ show, onClose, children}) {
   
   <div className ="fixed inset-0 bg-black text-center flex justify-center z-50 h-200
   xl:h-235">
-    <div className ="relative w-150 border-2 z-10 bg-white overflow-auto ">
+    <div className ="relative w-150 max-h-[90vh] border-2 z-10 bg-white overflow-y-auto ">
       {children}
       <button className ="border-2 text-lg w-20 mb-3 hover:cursor-pointer" 
       onClick= {onClose} > Stäng </button>
