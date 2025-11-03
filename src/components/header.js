5f13a41fb4701523510f6bd4885d0fe78533a23a
jane <jane.lehtola@afvagroup.com>
2025-11-03 17:34:45 +0100

    small changes

--- src/components/header.js
+++ src/components/header.js
@@ -16,20 +16,20 @@ return (
     src="book.jpg" alt="bild på bok"/>
     
     <div>
-      <p className ="absolute top-85 left-25 font-bold rotate-5
+      <p className ="absolute top-85 left-33 font-bold rotate-5
       sm:left-58 sm:text-lg
-      md:left-75 md:top-110 md:text-lg
-      lg:left-104 lg:top-110 lg:text-2xl
+      md:left-75 md:top-90 md:text-lg
+      lg:left-101 lg:top-70 lg:text-2xl
       xl:left-158 xl:top-60 xl:text-3xl 
       2xl:left-200 2xl:top-80 2xl:text-4xl">
         Welcome to my portfolio
         </p>
         
-        <p className ="text-xs top-100 left-19 rotate-5 overflow-hidden absolute 
+        <p className ="text-xs top-100 left-26 rotate-5 overflow-hidden absolute 
         animate-typing animate-blink-caret tracking-wider whitespace-nowrap
-        sm:left-47 sm:text-sm sm:font-bold
-        md:left-68 md:top-125
-        lg:left-101 lg:top-125 lg:text-sm
+        sm:left-47 sm:text-sm 
+        md:left-71 md:top-100 md:text-xs
+        lg:left-98 lg:top-85 lg:text-sm lg:font-bold
         xl:left-147 xl:top-85 xl:text-xl 
         2xl:left-185 2xl:top-110 2xl:text-2xl"> 
         Jane Lehtola - Junior Frontend Developer 
@@ -39,8 +39,8 @@ return (
         className ="hover:cursor-pointer animate-bounce absolute text-4xl
         top-115 left-41
         sm:left-75
-        md:left-92 md:top-140
-        lg:left-130 lg:top-140
+        md:left-92 md:top-110
+        lg:left-125 lg:top-100
         xl:left-190 xl:top-110 xl:text-5xl
         2xl:left-240 2xl:top-140 2xl:text-6xl">
           {arrow}
